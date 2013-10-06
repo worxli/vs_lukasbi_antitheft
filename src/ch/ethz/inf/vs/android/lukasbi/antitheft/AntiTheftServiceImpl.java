c651672b2ce76203f3967623721a975956afd4b4
yuhu <nico.previtali@gmail.com>
2013-10-06 17:31:03 +0200

    Anti-Theft service calling implemented

--- src/ch/ethz/inf/vs/android/lukasbi/antitheft/AntiTheftServiceImpl.java
+++ src/ch/ethz/inf/vs/android/lukasbi/antitheft/AntiTheftServiceImpl.java
@@ -3,12 +3,26 @@ package ch.ethz.inf.vs.android.lukasbi.antitheft;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.os.Vibrator;
+import android.util.Log;
 
 public class AntiTheftServiceImpl extends Service implements AntiTheftService {
 
 	//private Vibrator vib = null;
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+		Log.d("#A1", "antitheft service created");
+	}
 	
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		Log.d("#A1", "antitheft service destroyed");
+	}
+
 	@Override
 	public void startAlarm() {
 		// TODO Auto-generated method stub
--- src/ch/ethz/inf/vs/android/lukasbi/antitheft/MainActivity.java
+++ src/ch/ethz/inf/vs/android/lukasbi/antitheft/MainActivity.java
@@ -1,19 +1,21 @@
 package ch.ethz.inf.vs.android.lukasbi.antitheft;
 
-import ch.ethz.inf.vs.android.lukasbi.antitheft.eventlisteners.SensivityEventListener;
-import ch.ethz.inf.vs.android.lukasbi.antitheft.eventlisteners.TimeoutEventListener;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.ToggleButton;
+import ch.ethz.inf.vs.android.lukasbi.antitheft.eventlisteners.SensivityEventListener;
+import ch.ethz.inf.vs.android.lukasbi.antitheft.eventlisteners.TimeoutEventListener;
 
 public class MainActivity extends Activity {
 	
-	private int sensivity, timeout = 0;
+	/**
+	 * anti theft intent call
+	 */
+	private Intent antitheft;
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +32,27 @@ public class MainActivity extends Activity {
         
         sensivitySB.setOnSeekBarChangeListener(sensivityEL);
         timeoutSB.setOnSeekBarChangeListener(timeoutEL);
+        
+        // define antitheft service
+        antitheft = new Intent(this, AntiTheftServiceImpl.class);
+      //antitheft.putExtra("KEY1", "Value to be used by the service");
     }
+
+	/**
+	 * Eventhandler for the togglebutton
+	 */
+	public void onRunClicked (View v) {
+		// Is the toggle on?
+	    boolean on = ((ToggleButton) v).isChecked();
+	    
+	    if (on) {
+	    	// start anti theft service
+	    	this.startService(antitheft);
+	    } else {
+			// stop anti theft service
+			this.stopService(antitheft);
+	    }
+	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
